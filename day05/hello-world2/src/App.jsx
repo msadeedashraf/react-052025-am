57ccb6a7164af6948de1d971f157d1aebcc82a4d
msadeedashraf <157824309+msadeedashraf@users.noreply.github.com>
2025-05-16 13:13:01 -0400

    .

--- day05/hello-world2/src/App.jsx
+++ day05/hello-world2/src/App.jsx
@@ -1,8 +1,10 @@
 import './App.css'
 function App() {
+
+
   return (
     <>
-      <h1>Hello World</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam qui amet voluptas magni dignissimos, accusamus quam iure quis suscipit vel, deserunt illo perferendis obcaecati quas fugiat? Cum qui dolores harum rerum consequatur sint non quisquam nobis nam repudiandae fugit ut magnam porro aperiam, ipsum temporibus quod unde quibusdam natus eius asperiores. Unde consequatur dolor magnam reprehenderit reiciendis illo quod nisi rerum dolorum facere molestiae quae facilis porro sapiente fugiat, provident nihil praesentium! Nulla animi quasi sed ipsam perferendis est tenetur perspiciatis enim esse consectetur, voluptates sapiente in eum voluptas cum eaque, eligendi assumenda dolorem similique rerum. Molestias praesentium quaerat distinctio!</p>
     </>
   )
 }
